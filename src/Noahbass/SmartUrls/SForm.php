129f9e8bcd0d2f66d43017558636d0d5ea936f52
Noah Bass <bassjnoah@gmail.com>
2015-01-11 14:14:08 -0500

    updated smart forms: added model form and updated parameters variable

--- src/Noahbass/SmartUrls/SForm.php
+++ src/Noahbass/SmartUrls/SForm.php
@@ -5,14 +5,34 @@ use Illuminate\Support\Facades\Form;
 class SForm {
 
 	/**
-	 * Update action url to be protocal-relative..
+	 * Default parameters.
+	 * @var str $params
+	 */
+	protected $params = array('url' => '/');
+
+
+	/**
+	 * Update action url to be protocal-relative.
 	 * @var array $params
 	 *
 	 * @return str
 	 */
-	public static function open($params = array('url' => '/'))
+	public static function open($params)
 	{
 		return str_replace('http:', null, Form::open($params));
+	}
+
+
+	/**
+	 * Update action url in model form to be protocal-relative.
+	 * @var str $model
+	 * @var array $params
+	 *
+	 * @return str
+	 */
+	public static function model($model, $params)
+	{
+		return str_replace('http:', null, Form::model($model, $params));
 	}
 
 
